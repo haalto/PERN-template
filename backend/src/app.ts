6e65c1a90af22a54a4604bb903b0b2b07a3d50b5
Hannes_Aaltonen <hannes.aaltonen@tuni.fi>
2020-02-23 22:49:09 +0100

    Modified -env-example and removed sequelize

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,10 @@
-import { PORT, DB_URL } from './config/config'
+import { PORT } from './config/config'
 import express from 'express'
-import { Sequelize } from 'sequelize'
-
-const db = new Sequelize(DB_URL)
-db.authenticate()
-  .then(() => console.log('Connected to the database!'))
-  .catch((error: any) => console.log(`Error when connecting to the database: ${error}`))
 
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('hello')
+  res.send('Hello world!')
 })
 
 app.listen(PORT, () => {
